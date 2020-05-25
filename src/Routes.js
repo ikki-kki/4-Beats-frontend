d9887d08ac4f7804deff34fca74cf0f7077ed792
성정민 <seongjeongmin@seongjeongmin-ui-MacBook-Pro.local>
2020-05-25 17:33:39 +0900

    [Add]초기세팅 - 1차 프로젝트 1주차 스프린트

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+
+        </Switch>
+      </Router>
+    )
+  }
+}
+export default Routes;
+
