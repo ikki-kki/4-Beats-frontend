cdede2bcf933b13753bfd5cbdc0625993cb10a86
성정민 <seongjeongmin@seongjeongmin-ui-MacBook-Pro.local>
2020-05-28 11:28:54 +0900

    [Add, fix] pull request 수정및 footer추가 - header : 메인 nav 클릭 시 세부 메뉴 보여주기. - toggle 기능 구현 예정 - footer : ui 구현 완료 - 1440px 반응형 구현완료

--- src/components/Headers/MainHeader/HoverMenu/HoverMenu.js
+++ src/components/Headers/MainHeader/HoverMenu/HoverMenu.js
@@ -1,47 +1,37 @@
-import React, { Component } from 'react';
-import './Earphones.scss';
-import { headerData } from '../../../../utils/HeaderData/HeaderData';
-import { headerMain } from '../../../../utils/HeaderData/HeaderData';
+import React, { Component } from "react";
+import { headerData } from "../../../../utils/HeaderData/HeaderData";
+import { headerMain } from "../../../../utils/HeaderData/HeaderData";
+import "./HoverMenu.scss";
 class HoverMenu extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      class : ''
-    }
   }
 
   //hover시 state가 변하는 함수
   render() {
-    console.log("헤더 props: ", this.props)
-    console.log("헤더 데이터: ", headerData[this.props.name])
+    //console.log("헤더 props: ", this.props);
+    //console.log("헤더 데이터: ", headerData[this.props.name]);
     //console.log("헤더 클래스: ", this.state.class)
     return (
-      <div className="Earphones">
+      <div className="HoverMenu">
         <div className="main-img-box">
-          {
-            headerMain[this.props.name].data.map((data,i) => {
-              return(
-                <img className="main-img" key={i} src={data.img} alt=""/>
-              )
-            })
-          }
+          {headerMain[this.props.name].data.map((data, i) => {
+            return <img className="main-img" key={i} src={data.img} alt="" />;
+          })}
         </div>
         <div className="sub-imgs-container">
-          {
-            headerData[this.props.name].data.map((data, i) => {
-               return (
-                  <div className="sub-imgs-box" key={i} >
-                  <img className="sub-img" src={data.img} alt="" />
-                  <span className="sub-image-title">{data.name}</span>
-                </div>
-              )
-            }
-            )
-          }
+          {headerData[this.props.name].data.map((data, i) => {
+            return (
+              <div className="sub-imgs-box" key={i}>
+                <img className="sub-img" src={data.img} alt="" />
+                <span className="sub-image-title">{data.name}</span>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
   }
 }
 //sub-imgs-container를 맵돌고 그것들을 변수에 저장해서 넣기
-export default HoverMenu
+export default HoverMenu;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import MainFooter from '../../components/Footers/MainFooter/MainFooter'
+import MainHeader from '../../components/Headers/MainHeader/MainHeader'
 
 export class Main extends React.Component {
     render() {
         return (
             <div>
+                <MainHeader/>
+                <MainFooter/>
             </div>
         )
     }
