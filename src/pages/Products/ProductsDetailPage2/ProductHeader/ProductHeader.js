973f4e4dc0760c24e90cc3ea716e8901a4df9bdb
gogyungho <you@example.com>
2020-06-03 21:25:04 +0900

    [Add] Prodect Detail Page 레이아웃 작업

--- src/pages/Products/ProductsDetailPage2/ProductHeader/ProductHeader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./ProductDetailPage2.scss";
-
-class ProductHeader extends React.Component {
-  render() {
-    return <dic classNaem="ProductHeader"></dic>;
-  }
-}
-export default ProductHeader;
--- src/pages/Products/ProductsDetailPage2/ProductsDetailPage2.js
+++ src/pages/Products/ProductsDetailPage2/ProductsDetailPage2.js
@@ -1,38 +1,40 @@
 import React from "react";
 import Config from "../Config";
+import RedButton from "../../../components/Buttons/RedButton";
 import "./ProductsDetailPage2.scss";
 
 class ProductsDetailPage2 extends React.Component {
   render() {
     return (
       <div className="ProductsDetail_Pill">
-        <section className="Header">
-          <div className="HeaderWrap">
-            <div className="Text">
-              <div className="TextContainer">
-                <div className="TextInner">
-                  <h1>
-                    Beats Pill
-                    <sup>
-                      <b>+</b>
-                    </sup>
-                  </h1>
-                  <p>Portable Wireless Speaker</p>
-                  <div className="Price">
-                    <div className="PriceHolder">$179.95</div>
-                  </div>
+        <section className="HeaderWrap">
+          <div className="HeaderText">
+            <div className="TextInner">
+              <h1>
+                Beats Pill
+                <sup>
+                  <b>+</b>
+                </sup>
+              </h1>
+              <span>Portable Wireless Speaker</span>
+              <div className="PriceBuyWrap">
+                <div className="Price">$179.95</div>
+                <div className="BuyBtnWrap">
+                  <a
+                    href="https://www.apple.com/shop/product/ML4M2LL/A/beats-pill-portable-speaker-black?cid=app_Beats_PillPlus_PDP_US_AOS_BeatsPillPlus"
+                    className="BuyBtn"
+                  >
+                    <RedButton text={"Buy on"} link="/" />
+                  </a>
                 </div>
               </div>
             </div>
+          </div>
+
+          <div className="HeaderImg">
             <img src={Config.ProductsPillImg} alt="Main-Pill-img" />
           </div>
         </section>
-        <section></section>
-        <section></section>
-        <section></section>
-        <section></section>
-        <section></section>
-        <section></section>
       </div>
     );
   }
